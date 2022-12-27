72845cea7fce21cda8604ede12fd2f0f92646ac2
Jorge Martínez <jmmostazo.medac@gmail.com>
2022-12-27 13:30:59 +0100

    Tema7 ORM

---
+++ Tema7Hibernate/src/main/java/accesodatos/tema7hibernate/Tema7Hibernate.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package accesodatos.tema7hibernate;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Tema7Hibernate {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!!");
+    }
+}
